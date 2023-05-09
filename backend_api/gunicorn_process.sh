347d3e8659c3c6d22c6596ab68ca9f0b55cc4501
Roi <roezairi@gmail.com>
2023-05-10 00:34:31 +0700

    docker files fixes

--- backend_api/gunicorn_process.sh
+++ backend_api/gunicorn_process.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/usr/local/bin/gunicorn backend_api.wsgi --bind 0.0.0.0:8282 -D
+# to debug django application comment this launch and launch from debugger
+# /usr/local/bin/gunicorn backend_api.wsgi --bind 0.0.0.0:8282 -D
